966d6cffd73a9fe4e90e6d31ef26a666b5cacfee
basitali <168605157+Alibhai2420@users.noreply.github.com>
2024-12-16 14:48:31 +0500

    done

---
+++ js.js
@@ -0,0 +1,14 @@
+let todos = [];
+
+let a =prompt("What do you wanna do today")
+todos.push(a)
+
+let b =prompt("What do you wanna do today")
+todos.push(b)
+let c =prompt("What do you wanna do today")
+todos.push(c)
+let d =prompt("What do you wanna do today")
+todos.push(d)
+
+console.log("Your-todo-list");
+console.log(todos);
